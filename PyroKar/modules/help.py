427ede143d289ebcef1758cb4cee6ec87f09e026
ionnotXD <113478317+ionnotXD@users.noreply.github.com>
2022-11-29 10:16:41 +0700

    Update help.py

--- PyroKar/modules/help.py
+++ PyroKar/modules/help.py
@@ -28,12 +28,12 @@ async def module_help(client: Client, message: Message):
     elif not message.reply_to_message and len(cmd) == 1:
         ac = PrettyTable()
         ac.header = False
-        ac.title = "PyroKar-UserBot Modules"
+        ac.title = "PyroZu-UserBot Modules"
         ac.align = "l"
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @Karc0de × @obrolansuar •"
+            message, f"```{str(ac)}```\n• @Html12text × @kazusupportgrp •"
         )
         await message.reply(
             f"**Contoh Ketik** `{CMD_HANDLER}help afk` **Untuk Melihat Informasi Module**"
@@ -45,7 +45,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `{CMD_HANDLER}{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @Karc0de"
+            this_command += "© @Html12text"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
